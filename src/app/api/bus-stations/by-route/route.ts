f34f49accd57625976e06951de31b07b65a01927
MoonJuhan <codemike@naver.com>
2024-08-25 21:16:57 +0900

    feat: Add get bus stations by route api

---
+++ src/app/api/bus-stations/by-route/route.ts
@@ -0,0 +1,25 @@
+export async function GET(request: Request) {
+  const url = new URL(request.url)
+  const searchParams = url.searchParams
+
+  const serviceKey = process.env.NEXT_PUBLIC_OPEN_API_KEY as string
+
+  searchParams.append('serviceKey', serviceKey)
+  searchParams.append('resultType', 'json')
+
+  try {
+    const response = await fetch(
+      `http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?${searchParams.toString()}`,
+    )
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const json = await response.json()
+
+    return Response.json(json)
+  } catch (error) {
+    return error
+  }
+}
