eaa6a442d920162321fe8aeef7e2642b03b1d009
MoonJuhan <codemike@naver.com>
2024-08-26 15:03:12 +0900

    feat: Add show modal func when repoint marker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,9 @@
 'use client'
 
-import { useEffect } from 'react'
-import mapboxgl from 'mapbox-gl'
-import useStore from '@/stores'
 import Map from '@/components/Map'
 import ControlPanel from '@/components/ControlPanel'
 
 export default function App() {
-  const { map, marker, setMarker } = useStore()
-
-  const onClickMap = ({ lngLat }: mapboxgl.MapMouseEvent) => {
-    if (marker !== null) marker.remove()
-
-    const newMarker = new mapboxgl.Marker().setLngLat([lngLat.lng, lngLat.lat]).addTo(map)
-    setMarker(newMarker)
-  }
-
-  const addMapClickHandler = () => {
-    if (map !== null) map.on('click', onClickMap)
-  }
-
-  const removeMapClickHandler = () => {
-    if (map !== null) map.off('click', onClickMap)
-  }
-
-  useEffect(() => {
-    addMapClickHandler()
-
-    return () => {
-      removeMapClickHandler()
-    }
-  }, [map, marker])
-
   const onClickGetStationsByPosition = async () => {
     const { lng, lat } = marker?.getLngLat() || { lng: 0, lat: 0 }
 
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
 import mapboxgl from 'mapbox-gl'
 import useStore from '@/stores'
+import ModalBasic from './modal/ModalBasic'
 
 export default function Map() {
-  const { map, setMap } = useStore()
+  const { map, setMap, marker, setMarker } = useStore()
 
   const initMap = () => {
     if (map !== null) return
@@ -27,5 +29,52 @@ export default function Map() {
     initMap()
   }, [])
 
-  return <div id="map" className="w-screen h-screen" />
+  const onClickMap = ({ lngLat }: mapboxgl.MapMouseEvent) => {
+    if (marker !== null) {
+      setClickedPoint(lngLat)
+      setShowModal(true)
+      return
+    }
+
+    const newMarker = new mapboxgl.Marker().setLngLat([lngLat.lng, lngLat.lat]).addTo(map)
+    setMarker(newMarker)
+  }
+
+  useEffect(() => {
+    if (map !== null) map.on('click', onClickMap)
+
+    return () => {
+      if (map !== null) map.off('click', onClickMap)
+    }
+  }, [map, marker])
+
+  const [clickedPoint, setClickedPoint] = useState<mapboxgl.LngLat | null>(null)
+  const [showModal, setShowModal] = useState(false)
+  const onClickConfirm = () => {
+    setShowModal(false)
+
+    if (clickedPoint === null) return
+
+    marker.remove()
+
+    const newMarker = new mapboxgl.Marker().setLngLat([clickedPoint.lng, clickedPoint.lat]).addTo(map)
+    setMarker(newMarker)
+  }
+
+  return (
+    <>
+      <div id="map" className="w-screen h-screen" />
+      {createPortal(
+        <ModalBasic
+          show={showModal}
+          setShow={setShowModal}
+          title="확인하기"
+          buttons={[{ text: '확인', className: 'btn-primary', onClick: onClickConfirm }]}
+        >
+          새로운 위치를 지정하시겠습니까?
+        </ModalBasic>,
+        document.body,
+      )}
+    </>
+  )
 }
