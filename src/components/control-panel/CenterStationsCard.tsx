45f6c774c02f7f5e298594fedbf31cac43745c51
MoonJuhan <codemike@naver.com>
2024-08-26 23:42:38 +0900

    fix

--- src/components/control-panel/CenterStationsCard.tsx
+++ src/components/control-panel/CenterStationsCard.tsx
@@ -7,9 +7,9 @@ export default function CenterStationsCard() {
 
   return (
     centerStations.length > 0 && (
-      <div className="control-panel-card flex-col gap-2">
-        <span className="text-base">조회된 버스 정류장</span>
-        <div className="flex flex-col gap-2 max-h-60 overflow-y-auto">
+      <div className="control-panel-card h-2/5 flex-col gap-2">
+        <span className="text-base">조회된 버스 정류장 ({centerStations.length})</span>
+        <div className="flex flex-col gap-2 overflow-y-auto">
           {centerStations.map((station) => (
             <span key={station.stationId} className="text-sm">
               {station.stationNm}
--- src/components/control-panel/ControlPanel.tsx
+++ src/components/control-panel/ControlPanel.tsx
@@ -5,7 +5,7 @@ import CenterStationsCard from './CenterStationsCard'
 
 export default function ControlPanel() {
   return (
-    <div className="fixed left-4 top-4 z-10 flex flex-col gap-2">
+    <div className="fixed left-4 top-4 bottom-4 z-10 flex flex-col gap-2">
       <CenterOverviewCard />
       <CenterStationsCard />
     </div>
