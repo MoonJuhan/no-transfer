199dda08e9a5941a2354a1a22ddbb93fcfe576e2
MoonJuhan <codemike@naver.com>
2024-08-27 10:10:25 +0900

    feat: Add RoutesCard

--- src/components/control-panel/ControlPanel.tsx
+++ src/components/control-panel/ControlPanel.tsx
@@ -2,12 +2,14 @@
 
 import CenterOverviewCard from './CenterOverviewCard'
 import CenterStationsCard from './CenterStationsCard'
+import RoutesCard from './RoutesCard'
 
 export default function ControlPanel() {
   return (
     <div className="fixed left-4 top-4 bottom-4 z-10 flex flex-col gap-2">
       <CenterOverviewCard />
       <CenterStationsCard />
+      <RoutesCard />
     </div>
   )
 }
---
+++ src/components/control-panel/RoutesCard.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import useAppStore from '@/stores/app'
+import useMapStore from '@/stores/map'
+
+export default function CenterStationsCard() {
+  const { centerStations } = useMapStore()
+  const { setLoading } = useAppStore()
+  const isCenterStations = useMapStore(({ centerStations }) => centerStations.length > 0)
+
+  const onClickGetRoutesByStation = async () => {}
+
+  return (
+    isCenterStations && (
+      <div className="control-panel-card h-2/5 flex-col gap-2">
+        <div className="flex justify-between items-centers">
+          <span className="text-base">버스 경로 ({centerStations.length})</span>
+          <button className="btn-primary" onClick={onClickGetRoutesByStation}>
+            조회하기
+          </button>
+        </div>
+
+        <div className="flex flex-col gap-2 overflow-y-auto">
+          <span>Hello</span>
+        </div>
+      </div>
+    )
+  )
+}
